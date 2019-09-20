06de43e4fd47255a38570174a1f7c1d490d176f9
Martin Kierkegaard <martinkierkegaard@yahoo.com>
2019-09-20 14:39:23 +0200

    addede summary

--- OresundBronn/Car.cs
+++ OresundBronn/Car.cs
@@ -7,7 +7,11 @@ namespace OresundBronnLibrary
 {
     public class Car : Vehicle
     {
-        
+        /// <summary>
+        /// This is the car constructor
+        /// </summary>
+        /// <param name="licensePlate">licenseplate of the car, may not extend 7 char</param>
+        /// <param name="broBizzDiscount">discount if you have a broBizz</param>
         public Car(string licensePlate, bool broBizzDiscount = false) : base(licensePlate, broBizzDiscount)
         {
             LicensePlate = licensePlate;
